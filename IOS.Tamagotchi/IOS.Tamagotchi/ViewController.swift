e29e7431388b5a30b117642b3b981bf4fa69a3d0
AaronRexEton <rex.a@etoncollege.org.uk>
2020-01-14 10:37:39 +0000

    Begun Project and copied data and model from playground

---
+++ IOS.Tamagotchi/IOS.Tamagotchi/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS.Tamagotchi
+//
+//  Created by Rex, Aaron (SPH) on 14/01/2020.
+//  Copyright © 2020 Rex, Aaron (SPH). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
