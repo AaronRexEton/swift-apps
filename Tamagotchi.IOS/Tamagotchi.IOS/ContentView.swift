8e3027128a7175860724a68d6a0533cd28cc0d85
AaronRexEton <rex.a@etoncollege.org.uk>
2020-01-14 10:31:06 +0000

    Started Project copied data and model from previous Tamagotchi playground

---
+++ Tamagotchi.IOS/Tamagotchi.IOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tamagotchi.IOS
+//
+//  Created by Rex, Aaron (SPH) on 14/01/2020.
+//  Copyright © 2020 Rex, Aaron (SPH). All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tamagotchi.IOS/Tamagotchi.IOSTests/Tamagotchi_IOSTests.swift
@@ -0,0 +1,64 @@
+//
+//  Tamagotchi_IOSTests.swift
+//  Tamagotchi.IOSTests
+//
+//  Created by Rex, Aaron (SPH) on 14/01/2020.
+//  Copyright © 2020 Rex, Aaron (SPH). All rights reserved.
+//
+
+import XCTest
+@testable import Tamagotchi_IOS
+
+class Tamagotchi_IOSTests: XCTestCase {
+    
+    func testforAge() {
+        let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+        let tamAge = 0
+        XCTAssertEqual(myTamagotchi.getAge(), tamAge)
+        
+    }
+    
+    func testforHungry() {
+        let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+        let tamHunger = true
+        XCTAssertEqual(myTamagotchi.getHunger(), tamHunger)
+        
+    }
+    
+    func testforHappiness() {
+           let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+           let tamHappy = 3
+           XCTAssertEqual(myTamagotchi.getHappy(), tamHappy)
+           
+       }
+    func testForWeight() {
+        let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+        let tamWeight = 5.0
+        XCTAssertEqual(myTamagotchi.getWeight(), tamWeight)
+        
+    }
+    
+    func testForPlayGame() {
+        let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+        let testNeedattention = false
+        let testHappinessRank = 4
+        myTamagotchi.playGame()
+        XCTAssertEqual(myTamagotchi.getAttention(), testNeedattention)
+        XCTAssertEqual(myTamagotchi.getHappy(), testHappinessRank)
+        
+    }
+    
+    func testForEat() {
+        let myTamagotchi = Tamagotchi(name: "Bob", isMale: true)
+        let testHungry = false
+        let testHappinessRank = 4
+        let testWeight = 6.0
+        myTamagotchi.eat()
+        XCTAssertEqual(myTamagotchi.getHunger(), testHungry)
+        XCTAssertEqual(myTamagotchi.getWeight(), testWeight)
+        XCTAssertEqual(myTamagotchi.getHappy(), testHappinessRank)
+    }
+
+    
+
+}
